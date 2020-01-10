b95fcfaf6cef9ed5cf14eec065e7964df3f86a26
colin <chenzihu@gmail.com>
2020-01-10 15:41:21 +0800

    Simplize I2C check process, make it run fast

--- platform/i2c_f.sh
+++ platform/i2c_f.sh
@@ -5,9 +5,9 @@
 # exp:
 #i2c-2 20 AMP-MA12070-1
 #
+#NOTICE: I2C address using 7bit more and number as hex form !!!
 checklist=$1
 mkdir -p /data/factory
-buslist=/data/factory/i2c-buslist
 
 if [ -z "$1" ]; then
 	echo "FAIL: No Checklist input!"
@@ -15,38 +15,38 @@ elif [ ! -f $1 ]; then
 	echo "FAIL: Input file don't exit!"
 fi
 
-i2cdetect -l > $buslist
-
-busnum=$(wc -l < $buslist)
-
 # get line row context from file
 # get_lrf <line> <row> <file path>
 function get_lrf() {
 	awk -v line=$1 -v row=$2 'line==NR { print $row}' $3
 }
-echo "busnum : $busnum"
+# detect single I2C addres result
+# det_addr <I2c bus number> <I2C address>
+function check_i2c_addr() {
 
-while [ $busnum -gt 0 ]
-do
-	if [ $busnum -le 5 ]; then
-		i2cnum=$(get_lrf $busnum 1 $buslist | cut -c 5 )
-	else
-		 echo "FAIL: i2c bus number out of range"
-	fi
+	detfile=/data/factory/det-i2c
+	i2cnum=$(get_lrf $1 1 $2 | cut -c 5 )
+	i2caddr=$(get_lrf $1 2 $2)
 
-	detfile=/data/factory/det-i2c-$i2cnum
-	i2cdetect -y -r $i2cnum > $detfile
-
+	i2cdetect -y -r $i2cnum 0x$i2caddr 0x$i2caddr > $detfile
 	sed -i '1d' $detfile
 	sed -i 's/[^ ]*//' $detfile
-	busnum=$(($busnum-1))
-done
+	grep -q $i2caddr $detfile
+	if [ $? -eq 0 ]; then
+	        return 0
+	else
+		grep -q UU $detfile
+		if [ $? -eq 0 ]; then
+			return 0
+		else
+			return 1
+		fi
+	fi
+}
 
 function clean_up() {
 # delect temp file
-	rm -f /data/factory/det-i2c-*
-	rm -f $buslist
-
+	rm -f /data/factory/det-i2c
 }
 
 checknum=$(wc -l < $checklist)
@@ -54,11 +54,8 @@ checknum=$(wc -l < $checklist)
 echo "checknum : $checknum"
 while [ $checknum -gt 0 ]
 do
-	detfile=/data/factory/det-$(get_lrf $checknum 1 $checklist)
-	i2caddr=$(get_lrf $checknum 2 $checklist)
 	chipname=$(get_lrf $checknum 3 $checklist)
-
-	grep -q "$i2caddr" $detfile
+	check_i2c_addr $checknum $checklist
 	if [ $? -eq 0 ]; then
 		echo "PASS: chip <$chipname> addr <$i2caddr> detected!"
 	else
