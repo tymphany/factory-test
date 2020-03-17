aa0b5745ac00d5e0956b430c725f58f0af1848a5
Colin Chen <chenzihu@gmail.com>
2020-03-17 13:40:38 +0800

    Added bluetooth mac address change

--- platform/set_bt0_mac_addr_f.sh
+++ platform/set_bt0_mac_addr_f.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # set BT0 MAC ADDR to NVRAM
+# input formal:
+# xx:xx:xx:xx:xx:xx
+# exp:
+# 70:c9:4e:9b:64:11
 if [ -z "$1" ]; then
 	echo "FAIL: Please input BT0 MAC ADDR! "
 	exit 0
 fi
 
 nvram write BT0_MAC_ADDR $1
-
+rm /data/misc/bluetooth/.bt_nv.bin
+btnvtool -b $1
+adk-message-send 'connectivity_bt_disable{}'
+adk-message-send 'connectivity_bt_enable{}'
