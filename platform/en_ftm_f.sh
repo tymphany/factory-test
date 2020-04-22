d19c520b42bc3b604de3534bbe46667135bd3d1c
Colin Chen <chenzihu@gmail.com>
2020-04-22 20:11:24 +0800

    update guideline

--- platform/en_ftm_f.sh
+++ platform/en_ftm_f.sh
@@ -3,6 +3,15 @@
 
 insmod /usr/lib/modules/4.14.117/extra/wlan.ko
 ifconfig wlan0 up
+while true 
+do 
+	ifconfig | grep wlan0
+	if [ $? -eq 0 ]; then
+		break
+	fi
+	sleep 2
+	ifconfig wlan0 up
+done
 echo 5 > /sys/module/wlan/parameters/con_mode
 ftmdaemon -n -dd  &
 
