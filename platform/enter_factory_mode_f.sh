6d3d88a7602b3d7ee2c98133ddd1508cb59854b6
Colin Chen <chenzihu@gmail.com>
2020-04-24 10:52:05 +0800

    modify enter factory mode, don't move adk.audiomanager.db

--- platform/enter_factory_mode_f.sh
+++ platform/enter_factory_mode_f.sh
@@ -2,12 +2,17 @@
 # move system .db to backup fold to disable custom app for factory test, need reboot to apply
 mkdir -p /data/db_bk
 
-mv /data/*.db /data/db_bk
-#mv /data/adk.connectivity.*.db /data/db_bk
-#mv /data/adk.button.db /data/db_bk
-#mv /data/adk.led.db /data/db_bk
-#mv /data/allplay.db /data/db_bk
-#mv /data/spotify.db /data/db_bk
+#mv /data/*.db /data/db_bk
+mv /data/adk.connectivity.*.db /data/db_bk
+mv /data/adk.button.db /data/db_bk
+mv /data/adk.led.db /data/db_bk
+mv /data/allplay.db /data/db_bk
+mv /data/spotify.db /data/db_bk
+mv /data/adk.mediaplayer.db /data/db_bk
+mv /data/adk.rules.db /data/db_bk
+mv /data/adk.devices.db /data/db_bk
+mv /data/adk.voiceui.db /data/db_bk
+
 sync
 
 pkill led-manager
