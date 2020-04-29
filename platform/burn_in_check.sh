f2b799d31d87535ba81e151901ce53533e3c3a75
Colin Chen <chenzihu@gmail.com>
2020-04-29 13:15:30 +0800

    set pulse audio unmute and max vol during spdif on and burn in mode

--- platform/burn_in_check.sh
+++ platform/burn_in_check.sh
@@ -9,10 +9,26 @@ else
 	exit 0
 fi
 
+# check audio ready
+while true 
+do
+	pactl info | grep -q vt-64
+	if [ $? -eq 0 ]; then
+		break
+	else
+		sleep 1
+	fi
+done
+
+# pulse audio unmute
+adk-message-send 'audio_mute_set {mute : false}'
+# set pulse audio max volume
+adk-message-send 'audio_volume_set {value : 1}'
+
 #play pink noise
 while true 
 do
-	# set max volume
+	# set DSP max volume
 	i2ctransfer -y -f 2 w6@0x3b  0x00 0xa5  0x01 0x00 0x00 0x00
 
 	paplay /data/factory-test/pinknoise.wav
--- platform/spdif_on_f.sh
+++ platform/spdif_on_f.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # spdif on
+adk-message-send 'audio_mute_set {mute : false}'
+adk-message-send 'audio_volume_set {value : 1}'
 
 parec -d regular-spdif-in | pacat 
