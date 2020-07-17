df9ea6031403a7ac8e95f38435b8c6eda685ecde
zorro <zorro.lu@tymphany.com>
2020-07-17 18:18:03 +0800

    modify spidf script

--- platform/spdif_on_f.sh
+++ platform/spdif_on_f.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # spdif on
-adk-message-send 'audio_volume_set {value : 1}'
 
+adk-message-send 'audio_volume_set {value : 1}'
 tinymix set "PRI SPDIF TX Source" "Coaxial"
 pactl set-source-port 0 spdif-in
 pactl set-default-source 0
-gst-lanch-1.0 pulsesrc ! 'audio/x-raw, rate=(int)48000, channels=(int)2'! pulsesink
+gst-launch-1.0 pulsesrc ! 'audio/x-raw, rate=(int)48000, channels=(int)2'! pulsesink
