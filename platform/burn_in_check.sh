b30c5c08d3bdecf8e319b8e99bd65d72faaac787
zack-mf <limf1670@163.com>
2020-07-30 13:35:00 +0800

    add input arguments for burn_in times

--- platform/burn_in_check.sh
+++ platform/burn_in_check.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # check if system need to run burn in mode and start play pinknosic 
-
+times=0
 if [ -e /data/factory-test/burn_in.conf ]; then 
 	echo "Enter Burn in mode !"
+        times=`cat /data/factory-test/burn_in.conf`
 	rm /data/factory-test/burn_in.conf
 else
 	echo "Not burn in"
@@ -28,9 +29,15 @@ adk-message-send 'audio_volume_set {value : 1}'
 #play pink noise
 while true 
 do
+	if [ $times != "max" ];then
+		times=$((times-1))
+		if [ $times -lt 0 ]; then
+			exit 0
+		fi
+	fi		
+	
 	# set DSP max volume
-	i2ctransfer -y -f 2 w6@0x3b  0x00 0xa5  0x01 0x00 0x00 0x00
-
+	i2ctransfer -y -f 2 w6@0x3b 0x00 0xa3 0x01 0x00 0x00 0x00
 	paplay /data/factory-test/pinknoise.wav
 	if [ $? -eq 1 ]; then
 		sleep 5s
--- zepp/set_burn_in.sh
+++ zepp/set_burn_in.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # Set system to burn in mode, and run burn in proce once when reboot.
+# ./set_burn_in.sh max
+# $1 is times
+
 SHELL_FOLD=$(dirname $0)
 BASE_FOLD=$SHELL_FOLD/..
 
+echo $1 > $SHELL_FOLD/conf/burn_in.conf
 mkdir -p /data/factory-test
 cp -p $SHELL_FOLD/conf/burn_in.conf /data/factory-test/burn_in.conf
 cp -p $BASE_FOLD/platform/data/pinknoise.wav /data/factory-test/pinknoise.wav
+echo max > $SHELL_FOLD/conf/burn_in.conf
 
 if [ $? -eq 0 ]; then 
 	echo "OK"
