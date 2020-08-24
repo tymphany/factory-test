f24621efe90f25e7471cb61c37c93e12ebed56fc
zack-mf <limf1670@163.com>
2020-08-24 15:35:39 +0800

    mic record only for lvp15

--- lvp15/mic_record.sh
+++ lvp15/mic_record.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-# record 4 chnnal Audio to /data/factory-test/mic-4ch.wav
-
-SHELL_FOLD=$(dirname $0)
-BASE_FOLD=$SHELL_FOLD/..
+# record 3 chnnal Audio to /data/factory-test/mic-3ch.wav
+# Mic record, record 4 channel audio to /data/factory-test/mic-3ch.wav
 
 if [ -z "$1" ]; then
 	echo "Fail: Please input record time"
 	exit 0
 fi
 
-$BASE_FOLD/platform/mic_record_f.sh $1
+recordpath=/data/factory-test/mic-3ch.wav
+mkdir -p /data/factory-test
+if [ -e $recordpath ]; then
+        rm $recordpath
+fi
+
+tinycap $recordpath -c 3 -t $1
 
 echo "OK"
--- lvp15/mic_record_init.sh
+++ lvp15/mic_record_init.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 # init record
+# mic record setting
 
-SHELL_FOLD=$(dirname $0)
-BASE_FOLD=$SHELL_FOLD/..
+tinymix set "MultiMedia1 Mixer VA_CDC_DMA_TX_0" "1" 
+tinymix set "VA_CDC_DMA_TX_0 Channels" "Three"
+tinymix set "VA_AIF1_CAP Mixer DEC0" "1"
+tinymix set "VA_AIF1_CAP Mixer DEC1" "1"
+tinymix set "VA_AIF1_CAP Mixer DEC2" "1"
+tinymix set "VA DMIC MUX0" "DMIC0"
+tinymix set "VA DMIC MUX1" "DMIC1"
+tinymix set "VA DMIC MUX2" "DMIC2"
 
-$BASE_FOLD/platform/mic_record_init_f.sh 
+echo "OK"
 
