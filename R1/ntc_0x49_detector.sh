7bb04fc379b8ebe769530415c9654c8efdc97b77
zack-mf <limf1670@163.com>
2020-09-14 16:08:48 +0800

    add r1 script

---
+++ R1/ntc_0x49_detector.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+#
+#       NTC_0x49_DETECT
+#
+
+echo 43 > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio43/direction
+GPIO_STATUS=`cat /sys/class/gpio/gpio43/value`
+if [ ${GPIO_STATUS} -eq 0 ]; then
+        echo "[Exception Occured]: GPIO TH_DET already stay LOW status"
+                echo "[Exception Occured]: Check GPIO TH_DET before detect"
+                echo 43 > /sys/class/gpio/unexport
+                exit
+fi
+
+i2ctransfer -y -f 5 w3@0x49 0x03 0x05 0x00
+i2ctransfer -y -f 5 w1@0x49 0x01 r2
+sleep 1s
+
+GPIO_STATUS=`cat /sys/class/gpio/gpio43/value`
+if [ ${GPIO_STATUS} -eq 0 ]; then
+        echo "[NTC 0x49 Detect]: OK"
+fi
+
+i2ctransfer -y -f 5 w3@0x49 0x03 0x7f 0x00
+i2ctransfer -y -f 5 w1@0x49 0x01 r2
+sleep 1s
+
+GPIO_STATUS=`cat /sys/class/gpio/gpio43/value`
+#if [ ${GPIO_STATUS} -eq 1 ]; then
+#        echo "[NTC 0x49 Detect]: Over"
+#fi
+
+echo 43 > /sys/class/gpio/unexport
+
---
+++ R1/read_bt_name.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# read bt name 
+
+adkcfg -f /data/adk.connectivity.bt.db read connectivity.bt.device_name
