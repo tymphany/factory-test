e3401db36b01a6e716469e2149f90a2fe8f25631
zack-mf <limf1670@163.com>
2020-09-24 17:22:02 +0800

    update bt name change script

--- lvp15/rename_bt.sh
+++ lvp15/rename_bt.sh
@@ -2,7 +2,16 @@
 #
 # rename bt 
 
-adkcfg -f /data/adk.connectivity.bt.db write connectivity.bt.device_name "$1" --ignore
+bt_name_path=/etc/bluetooth
+mount -o remount,rw /
+cp $bt_name_path/bt_app.conf $bt_name_path/bt_app.conf.back
+sed -i "11c BtLocalDeviceName=$1" $bt_name_path/bt_app.conf.back
+cp $bt_name_path/bt_app.conf.back $bt_name_path/bt_app.conf
+rm $bt_name_path/bt_app.conf.back
+systemctl restart btapp
+mount -o remount,ro /
+
+#adkcfg -f /data/adk.connectivity.bt.db write connectivity.bt.device_name "$1" --ignore
 
 sleep 0.5s
 
