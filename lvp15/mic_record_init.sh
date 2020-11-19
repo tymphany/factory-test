0965fb51b56c3a39bf82e7dc661f7a41a0c7cf49
HokyGUAN <hoky.guan@tymphany.com>
2020-11-19 17:04:33 +0800

    Active 3 mics

--- lvp15/mic_record_init.sh
+++ lvp15/mic_record_init.sh
@@ -5,11 +5,11 @@
 tinymix set "MultiMedia1 Mixer VA_CDC_DMA_TX_0" "1" 
 tinymix set "VA_CDC_DMA_TX_0 Channels" "Three"
 tinymix set "VA_AIF1_CAP Mixer DEC0" "1"
-tinymix set "VA_AIF1_CAP Mixer DEC1" "1"
+tinymix set "VA_AIF1_CAP Mixer DEC2" "1"
 tinymix set "VA_AIF1_CAP Mixer DEC4" "1"
 tinymix set "VA DMIC MUX0" "DMIC0"
-tinymix set "VA DMIC MUX1" "DMIC1"
 tinymix set "VA DMIC MUX2" "DMIC2"
+tinymix set "VA DMIC MUX4" "DMIC4"
 
 echo "OK"
 
