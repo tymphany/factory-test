e969d820e6b6158a447733fe11726b1f0d2e9f31
alexey.abdulov <Alexey.Abdulov@tymphany.com>
2020-11-23 10:47:50 +0800

    [DESC]: Added feedback for lantern on/off. Add amps mute

--- r1/set_lantern_on.sh
+++ r1/set_lantern_on.sh
@@ -8,3 +8,5 @@ echo 255 > /sys/class/leds/Lantern_W2_r/brightness
 echo 255 > /sys/class/leds/Lantern_W3_b/brightness
 echo 255 > /sys/class/leds/Lantern_W3_g/brightness
 echo 255 > /sys/class/leds/Lantern_W3_r/brightness
+
+echo "done"
