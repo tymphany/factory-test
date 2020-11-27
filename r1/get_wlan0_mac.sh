79572d2847e505da5770880a69e861ed50657482
alexey.abdulov <Alexey.Abdulov@tymphany.com>
2020-11-27 16:57:44 +0800

    [DESC]: Show default MAC

--- r1/get_wlan0_mac.sh
+++ r1/get_wlan0_mac.sh
@@ -5,5 +5,5 @@ SHELL_FOLD=$(dirname $0)
 BASE_FOLD=$SHELL_FOLD/..
 FILE_PATH=/persist/factory/product-message/r1.txt
 
-$BASE_FOLD/platform/get_wlan0_mac_addr_f.sh $FILE_PATH
+$BASE_FOLD/r1/get_wlan0_mac_addr_f.sh $FILE_PATH
 exit $?
