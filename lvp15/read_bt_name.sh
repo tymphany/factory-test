10bc2913421e8ba3e9abf581bac8fd0b22d15332
zack-mf <limf1670@163.com>
2020-12-10 10:44:46 +0800

    modify lv script: read bt name

--- lvp15/read_bt_name.sh
+++ lvp15/read_bt_name.sh
@@ -2,4 +2,6 @@
 #
 # read bt name 
 
-adkcfg -f /data/adk.connectivity.bt.db read connectivity.bt.device_name
+#adkcfg -f /data/adk.connectivity.bt.db read connectivity.bt.device_name
+
+cat /etc/bluetooth/bt_app.conf | grep -r BtLocalDeviceName | awk -F "=" '{print $NF}'
