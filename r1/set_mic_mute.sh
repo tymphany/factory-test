0cf8cf29837f0f45a305e4f9eab6c641133150a6
alexey.abdulov <Alexey.Abdulov@tymphany.com>
2020-12-28 17:32:54 +0800

    [DESC]: Set mic mute update

--- r1/set_mic_mute.sh
+++ r1/set_mic_mute.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # read mic mute state
 [ ! -d "/sys/class/gpio/gpio48" ] && echo 48 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio48/direction
 echo 0 > /sys/class/gpio/gpio48/value
 echo 1 > /sys/class/gpio/gpio48/value
 echo OK
