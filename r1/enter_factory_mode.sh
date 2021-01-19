912b39dba783f1d62dd6be34e53699d742cb3eec
Ryder Lee <ryder.lee@tymphany.com>
2021-01-19 17:47:19 +0800

    disable amps at startup to reduce current peak at startup

--- r1/enter_factory_mode.sh
+++ r1/enter_factory_mode.sh
@@ -3,6 +3,8 @@
 SHELL_FOLD=$(dirname $0)
 BASE_FOLD=$SHELL_FOLD/..
 
+#Disable amplifier
+echo "" > /dev/shm/disTone
 adk-message-send 'system_mode_management {name: "Test::ForceOffLeds"}'
 
 echo "OK"
--- r1/exit_factory_mode.sh
+++ r1/exit_factory_mode.sh
@@ -3,6 +3,8 @@
 SHELL_FOLD=$(dirname $0)
 BASE_FOLD=$SHELL_FOLD/..
 
+#enble amplifier at startup
+rm -f /dev/shm/disTone
 adk-message-send 'system_mode_management {name: "Test::ForceOnLeds"}'
 
 echo "OK"
