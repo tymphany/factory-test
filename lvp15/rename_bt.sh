519db540175f521b84629e968d03791efb9d53c4
zack-mf <limf1670@163.com>
2021-02-27 15:18:07 +0800

    fix perf can't rename bt

--- lvp15/rename_bt.sh
+++ lvp15/rename_bt.sh
@@ -3,13 +3,11 @@
 # rename bt 
 
 bt_name_path=/etc/bluetooth
-mount -o remount,rw /
-cp $bt_name_path/bt_app.conf $bt_name_path/bt_app.conf.back
-sed -i "11c BtLocalDeviceName=$1" $bt_name_path/bt_app.conf.back
-cp $bt_name_path/bt_app.conf.back $bt_name_path/bt_app.conf
-rm $bt_name_path/bt_app.conf.back
+cp $bt_name_path/bt_app.conf /data
+sed -i "11c BtLocalDeviceName=$1" /data/bt_app.conf
+cp /data/bt_app.conf $bt_name_path
+rm /data/bt_app.conf
 systemctl restart btapp
-mount -o remount,ro /
 
 if [ -e /data/adk.connectivity.bt.db ];then
 	adkcfg -f /data/adk.connectivity.bt.db write connectivity.bt.device_name "$1" --ignore
