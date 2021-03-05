29d8953deb37c92fcf6f4ba67c78cad2c5351c35
Ryder Lee <ryder.lee@tymphany.com>
2021-03-05 14:08:55 +0800

    change dspflow parameter for splay

--- r1/play_file.sh
+++ r1/play_file.sh
@@ -1,4 +1,4 @@
 adk-message-send 'system_mode_management {name:"audio_Streaming"}'
-gst-launch_64-1.0 filesrc location= $1 ! wavparse ! audioconvert mix-matrix="<<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>,<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>>" ! audio/x-raw,channels=4 ! flowdspgst port=50013 ! pulsesink
+gst-launch_64-1.0 filesrc location= $1 ! wavparse ! audioconvert mix-matrix="<<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>,<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>>" ! audio/x-raw,channels=4 ! flowdspgst port=50013 name="flowdsp" ! pulsesink
 adk-message-send 'system_mode_management {name:"audio_Stoppped"}'
 
