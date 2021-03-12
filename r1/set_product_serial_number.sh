42a5e69b4e710b38ca6eeaabbf17e3f973c33e76
Jerry <348428118@qq.com>
2021-03-12 19:41:44 +0800

    Update setting product serial number code

--- r1/set_product_serial_number.sh
+++ r1/set_product_serial_number.sh
@@ -2,7 +2,12 @@
 
 INFO_PATH=/persist/factory/product-message
 
-sed -i "5c PRODUCT_SN $1" $INFO_PATH/r1.txt
-
-echo OK
+#judge if $1 is not empty 
+if [ ! -n "$1" ]
+then
+	echo FAIL
+else
+	sed -i "5c PRODUCT_SN $1" $INFO_PATH/r1.txt
+	echo OK	
+fi
 
