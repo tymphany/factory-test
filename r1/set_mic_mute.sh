4e400febfa8fe818a56343d1d943794d5d02dd6c
Jerry <348428118@qq.com>
2021-03-16 13:37:17 +0800

    Update the code for DVT Led HW Change

--- r1/set_mic_mute.sh
+++ r1/set_mic_mute.sh
@@ -3,6 +3,10 @@
 [ ! -d "/sys/class/gpio/gpio86" ] && echo 86 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio86/direction
 echo 0 > /sys/class/gpio/gpio86/value
+sleep 1s
+#Convert mute state L->H for mute from EVT to DVT
 echo 1 > /sys/class/gpio/gpio86/value
+echo 0 > /sys/class/gpio/gpio86/value
+
 echo OK
 
--- r1/set_product_serial_number.sh
+++ r1/set_product_serial_number.sh
@@ -2,9 +2,10 @@
 
 INFO_PATH=/persist/factory/product-message
 
-#judge if $1 is not empty 
+#judge whether $1 is empty or not 
 if [ ! -n "$1" ]
 then
+	#$1 is empty
 	echo FAIL
 else
 	sed -i "5c PRODUCT_SN $1" $INFO_PATH/r1.txt
