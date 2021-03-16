b2ff52880d62deed3cbcfc070b71570803cefe6f
Jerry <348428118@qq.com>
2021-03-16 16:55:57 +0800

    Update set_led.sh for indicating led setting result

--- r1/set_led.sh
+++ r1/set_led.sh
@@ -17,30 +17,11 @@ then
 	echo "FAIL: Wrong Brightness Value"
 	exit 1
 else
-	if 	[[ $1 == "ACTION_g" ]] || [[ $1 == "ACTION_r" ]] || \
-		[[ $1 == "CONNECT_b" ]] || [[ $1 == "CONNECT_g" ]] || [[ $1 == "CONNECT_r" ]] || \
-		[[ $1 == "Lantern_g" ]] || [[ $1 == "Lantern_r" ]] || \
-		[[ $1 == "PLAYPAUSE_g" ]] || [[ $1 == "PLAYPAUSE_r" ]] || \
-		[[ $1 == "USB_r" ]] || \
-		[[ $1 == "VOL+_g" ]] || [[ $1 == "VOL+_r" ]] || \
-		[[ $1 == "VOL-_g" ]] || [[ $1 == "VOL-_r" ]] || \
-		[[ $1 == "Mute_g" ]] || [[ $1 == "Mute_r" ]] || \
-		[[ $1 == "Lantern_W1_g" ]] || [[ $1 == "Lantern_W1_r" ]] || \
-		[[ $1 == "Lantern_W2_b" ]] || [[ $1 == "Lantern_W2_g" ]] || [[ $1 == "Lantern_W2_r" ]] || \
-		[[ $1 == "Lantern_W3_b" ]] || [[ $1 == "Lantern_W3_g" ]] || [[ $1 == "Lantern_W3_r" ]] || \
-		[[ $1 == "Lantern_led1_r" ]] || [[ $1 == "Lantern_led2_r" ]] || \
-		[[ $1 == "Lantern_led3_r" ]] || [[ $1 == "Lantern_led4_r" ]] || [[ $1 == "Lantern_led5_r" ]] || \
-		[[ $1 == "Lantern_led6_r" ]] || [[ $1 == "Lantern_led7_r" ]] || [[ $1 == "Lantern_led8_r" ]] || \
-		[[ $1 == "Lantern_led1_g" ]] || [[ $1 == "Lantern_led2_g" ]] || \
-		[[ $1 == "Lantern_led3_g" ]] || [[ $1 == "Lantern_led4_g" ]] || [[ $1 == "Lantern_led5_g" ]] || \
-		[[ $1 == "Lantern_led6_g" ]] || [[ $1 == "Lantern_led7_g" ]] || [[ $1 == "Lantern_led8_g" ]] || \
-		[[ $1 == "Lantern_led1_b" ]] || [[ $1 == "Lantern_led2_b" ]] || \
-		[[ $1 == "Lantern_led3_b" ]] || [[ $1 == "Lantern_led4_b" ]] || [[ $1 == "Lantern_led5_b" ]] || \
-		[[ $1 == "Lantern_led6_b" ]] || [[ $1 == "Lantern_led7_b" ]] || [[ $1 == "Lantern_led8_b" ]]
+	echo $2 > /sys/class/leds/$1/brightness
+	if [ "$?" -ne 0 ]
 	then
-		echo "OK"
-		echo $2 > /sys/class/leds/$1/brightness
+		echo "FAIL: Wrong LED Name"
 	else
-		echo "FAIL: Wrong LED name"
+		echo "OK"
 	fi
 fi
