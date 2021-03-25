e8322ae517d93b299f6cacd343eda31002b5442b
Jerry <348428118@qq.com>
2021-03-25 10:50:13 +0800

    Update the mic record LED indication

--- r1/mic_record_init.sh
+++ r1/mic_record_init.sh
@@ -1,6 +1,16 @@
-echo 254 > /sys/class/leds/ACTION_r/brightness
 echo 254 > /sys/class/leds/ACTION_g/brightness
+echo 254 > /sys/class/leds/ACTION_r/brightness
+echo 0 > /sys/class/leds/VOL-_g/brightness
+echo 0 > /sys/class/leds/VOL-_r/brightness
+echo 0 > /sys/class/leds/PLAYPAUSE_g/brightness
+echo 0 > /sys/class/leds/PLAYPAUSE_r/brightness
+echo 0 > /sys/class/leds/VOL+_g/brightness
+echo 0 > /sys/class/leds/VOL+_r/brightness
+echo 0 > /sys/class/leds/Mute_g/brightness
+echo 0 > /sys/class/leds/Mute_r/brightness
+
 /etc/factory-test/platform/mic_record_init_f.sh
-echo 0 > /sys/class/leds/ACTION_r/brightness
-echo 0 > /sys/class/leds/ACTION_g/brightness
+
+echo 254 > /sys/class/leds/VOL-_g/brightness
+echo 254 > /sys/class/leds/VOL-_r/brightness
 
