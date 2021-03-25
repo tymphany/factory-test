c5bdce00299b6409bac08175f468ab1215bb1dd7
Jerry <348428118@qq.com>
2021-03-25 13:21:52 +0800

    Mic Record indication,  Merge the platform/mic_recrod_f.sh to mic_record_start.sh

--- r1/mic_record_start.sh
+++ r1/mic_record_start.sh
@@ -1,5 +1,20 @@
-/etc/factory-test/platform/mic_record_f.sh $1
+#/bin/sh
+# Mic record, record 4 channel audio to /data/factory-test/mic-4ch.wav
+recordpath=/data/factory-test/mic-4ch.wav
+mkdir -p /data/factory-test
+if [ -e $recordpath ]; then
+	rm $recordpath
+fi
+
+#start mic recording
+echo 254 > /sys/class/leds/PLAYPAUSE_g/brightness
+echo 254 > /sys/class/leds/PLAYPAUSE_r/brightness
+
+tinycap $recordpath -c 4 -t $1
+
+
 echo 254 > /sys/class/leds/VOL+_g/brightness
 echo 254 > /sys/class/leds/VOL+_r/brightness
+
 #wav file is at: /data/factory-test/mic-3ch.wav
 #adb pull /data/factory-test/mic-4ch.wav .\
