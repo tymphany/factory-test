510e175890ed718bc033853b696efa4457107058
alexey.abdulov <Alexey.Abdulov@tymphany.com>
2021-03-30 14:19:00 +0800

    [DESC]: Add delay after unmute amps before playback. Critical for short swipe tracks.

--- r1/play_file.sh
+++ r1/play_file.sh
@@ -1,4 +1,5 @@
 adk-message-send 'system_mode_management {name:"audio_Streaming"}'
+sleep 0.7
 gst-launch_64-1.0 filesrc location= $1 ! wavparse ! audioconvert mix-matrix="<<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>,<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>>" ! audio/x-raw,channels=4 ! flowdspgst port=54010 name="flowdsp" ! pulsesink
 adk-message-send 'system_mode_management {name:"audio_Stoppped"}'
 
