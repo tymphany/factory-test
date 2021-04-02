455a17c145dbeb3a3e20c6e17ee8e13ba471f8f7
Jerry <348428118@qq.com>
2021-04-02 19:49:12 +0800

    Update Enter Factory mode for turn off usb/playpause LED

--- r1/enter_factory_mode.sh
+++ r1/enter_factory_mode.sh
@@ -8,3 +8,10 @@ echo "" > /dev/shm/testing
 adk-message-send 'system_mode_management {name: "Test::ForceOffLeds"}'
 adk-message-send 'system_mode_management {name: "Test::enter_test_mode"}'
 echo "OK"
+
+
+#disable LED Pulsing function
+#turn off usb led
+adk-message-send 'led_start_pattern {pattern:33}'
+#turn off action/vol-/playpause/vol+ led
+adk-message-send 'led_start_pattern {pattern:16}'
