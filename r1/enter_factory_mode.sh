7c4c1d10577356e9cfcbb1c7ccba7729932625cf
Jerry <348428118@qq.com>
2021-04-09 17:26:40 +0800

    Change the code of enter factory test mode script

--- r1/enter_factory_mode.sh
+++ r1/enter_factory_mode.sh
@@ -5,13 +5,10 @@ BASE_FOLD=$SHELL_FOLD/..
 
 #Disable amplifier
 echo "" > /dev/shm/testing
+#turn off action/vol-/playpause/vol+/Lantern/Mute
 adk-message-send 'system_mode_management {name: "Test::ForceOffLeds"}'
-adk-message-send 'system_mode_management {name: "Test::enter_test_mode"}'
-echo "OK"
-
-
-#disable LED Pulsing function
 #turn off usb led
 adk-message-send 'led_start_pattern {pattern:33}'
-#turn off action/vol-/playpause/vol+ led
-adk-message-send 'led_start_pattern {pattern:16}'
+#jump in factory test mode
+adk-message-send 'system_mode_management {name: "Test::enter_test_mode"}'
+echo "OK"
