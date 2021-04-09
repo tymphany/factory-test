36ac36f0ecee509560a0cd2e1e9b0bf098cce97a
Jerry <348428118@qq.com>
2021-04-09 11:47:23 +0800

    Update the volume range, same with get bt volume. from -120 to 0.

--- r1/play_file.sh
+++ r1/play_file.sh
@@ -1,28 +1,16 @@
 #define 
 #default volume value is max value
-sound_value="0"
-input_value=$2
-
-if [ $input_value -ge 10 ];then
-	sound_value="0"
-elif [ $input_value -ge 9 ];then
-	sound_value="-10"	
-elif [ $input_value -ge 7 ];then
-	sound_value="-20"
-elif [ $input_value -ge 5 ];then
-	sound_value="-40"
-elif [ $input_value -ge 3 ];then
-	sound_value="-60"
-elif [ $input_value -ge 1 ];then
-	sound_value="-80"
+if [ $2 -gt 0 ] ||  [ $2 -lt -120 ]
+then
+	echo "volume range -120 to 0"
+	exit
 else
-	sound_value="-120"
+	echo "OK"
 fi
 
 adk-message-send 'system_mode_management {name:"audio_Streaming"}'
 sleep 0.7
-
-gst-launch_64-1.0 filesrc location= $1 ! wavparse ! audioconvert mix-matrix="<<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>,<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>>" ! audio/x-raw,channels=4 ! flowdspgst port=54010 msg=set/GAIN_ST_1/gain/$sound_value/   name="flowdsp" ! pulsesink
-
+gst-launch_64-1.0 filesrc location= $1 ! wavparse ! audioconvert mix-matrix="<<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>,<(float)1.0,(float)0.0>,<(float)0.0,(float)1.0>>" ! audio/x-raw,channels=4 ! flowdspgst port=54010 msg=set/GAIN_ST_1/gain/$2/   name="flowdsp" ! pulsesink
 adk-message-send 'system_mode_management {name:"audio_Stoppped"}'
+
 
