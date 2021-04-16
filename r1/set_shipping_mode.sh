970a7ce830923c55b861339f0fe5bf32b4883b2d
Ryder Lee <ryder.lee@tymphany.com>
2021-04-16 08:59:50 +0800

    sync before set shipment

--- r1/set_shipping_mode.sh
+++ r1/set_shipping_mode.sh
@@ -5,9 +5,12 @@ SHELL_FOLD=$(dirname $0)
 BASE_FOLD=$SHELL_FOLD/..
 
 i2ctransfer -y 5 w3@0x6b 0x00 0x2e 0x86
+/etc/exitscripts/board-exit.sh
+echo s > /proc/sysrq-trigger
 sleep 0.5
 /etc/factory-test/r1/set_bat_shutdown.sh
 
+#below script won't be run as the system will lose power after above script.
 sleep 1
 
 $BASE_FOLD/platform/set_shutdown_f.sh
