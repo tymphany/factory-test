f27cc617e87305bb627bc3f6e971ba63802ec588
Alexey Abdulov <Alexey.Abdulov@tymphany.com>
2021-05-17 18:06:37 +0800

    [DESC]: Factory reset misprint

--- r1/factory_reset.sh
+++ r1/factory_reset.sh
@@ -3,8 +3,8 @@
 
 #enble amplifier at startup
 rm /data/wpa_supplicant_home.conf
-adkcfg -f data/adk.connectivity.states.db write connectivity.states.wifi_onboarded 0 --ignore
-adkcfg -f data/adk.device.db reset device.friendly_name
+adkcfg -f /data/adk.connectivity.states.db write connectivity.states.wifi_onboarded 0 --ignore
+adkcfg -f /data/adk.device.db reset device.friendly_name
 /etc/initscripts/get_friendly_name.sh 2
 adk-message-send 'connectivity_bt_clearpairedlist{}'
 rm /data/bt_last_connected_address.txt
