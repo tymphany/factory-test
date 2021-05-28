a95c0950aaa4adafddea1493378114140c33f55e
zack-mf <limf1670@163.com>
2021-05-28 13:31:48 +0800

    optimize hold script

--- lvp15/disable_ota_soc_hold.sh
+++ lvp15/disable_ota_soc_hold.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-# set low the cypress pin
+#set high cypress pin
 
 i2cset -f -y 2 0x08 0x0d 0
 sleep 1
+while true
+do
+	HOLD=`i2cget -f -y 2 0x08 0x0d`
+	if [ $HOLD == 0x00 ];then
+		break;
+	else
+		i2cset -f -y 2 0x08 0x0d 0
+	fi
+	sleep 1
+done
 echo "OK"
--- lvp15/enabled_ota_soc_hold.sh
+++ lvp15/enabled_ota_soc_hold.sh
@@ -3,4 +3,14 @@
 
 i2cset -f -y 2 0x08 0x0d 1
 sleep 1
+while true
+do
+	HOLD=`i2cget -f -y 2 0x08 0x0d`
+	if [ $HOLD == 0x01 ];then
+		break
+	else
+		i2cset -f -y 2 0x08 0x0d 1
+	fi
+	sleep 1
+done
 echo "OK"
